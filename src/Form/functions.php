d646ee03e44f6a1f050127201e7fc829b7fe4f49
Wade Teng <wadedang@gmail.com>
2022-05-31 18:32:14 +0800

    Update functions.php
    fix the issue of self-signed SSL cert

--- src/Form/functions.php
+++ src/Form/functions.php
@@ -5,19 +5,25 @@ function read_sitemap($base_url){
     $url_list = array();
     $doc = new DOMDocument();
     libxml_use_internal_errors(true);
-    $html = file_get_contents($base_url . "/sitemap.xml");
-    $doc->loadHTML($html);
-    $items = $doc->getElementsByTagName('loc');
-    if(count($items) > 0)
-    {
-        foreach ($items as $tag1)
+
+    $context = [ 'http' => [ 'timeout' => 2, 'method' => 'GET' ], 'ssl' => [ 'verify_peer' => false, 'allow_self_signed'=> true ] ];
+    $context = stream_context_create($context);
+    $html = file_get_contents($base_url . "/sitemap.xml", false, $context);
+
+    if (!empty($html)){
+        $doc->loadHTML($html);
+        $items = $doc->getElementsByTagName('loc');
+        if(count($items) > 0)
         {
-            array_push($url_list, $tag1->nodeValue);
+            foreach ($items as $tag1)
+            {
+                array_push($url_list, $tag1->nodeValue);
+            }
         }
-    }
-    else
-    {
-        array_push($url_list, $doc->saveHTML());
+        else
+        {
+            array_push($url_list, $doc->saveHTML());
+        }
     }
     return $url_list;
 }
@@ -26,10 +32,10 @@ function read_sitemap($base_url){
 function curl($url){
     $ci = curl_init();
     curl_setopt($ci , CURLOPT_URL , $url);
-    #curl_setopt($ch, CURLOPT_USERAGENT, "Google Bot");
     $result = curl_exec($ci);
     curl_close($ci);
 }
 
-#print_r(read_sitemap());
+// print_r(read_sitemap('https://example.com'));
 ?>
+
